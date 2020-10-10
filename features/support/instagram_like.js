6145d0c8d5624288eead275167507771e12b9c41
Berk <berk@Berk-MacBook-Air.local>
2020-10-10 15:25:21 +0300

    güncellendi

--- features/support/instagram_like.js
+++ features/support/instagram_like.js
@@ -3,58 +3,48 @@ const { Given, When, Then, defineSupportCode } = require('cucumber');
 
 const driver = new Builder().forBrowser("chrome").build();
 
-Given('I visit instagram', async function() {
+Given('I visit instagram',{timeout: 10 * 1000}, async function() {
     await driver.get("https://www.instagram.com");  
 });
 
-When('I see log in form', async function() {
+When('I see log in form',{timeout: 10 * 1000}, async function() {
     const condition = until.elementLocated(By.name('username'));
-    await driver.wait(condition,6000);
+    await driver.wait(condition);
 });
 
 Then('I log in with {string} username and {string} password', async function(user, pass){
     const username = await driver.findElement(By.name("username"));
-    username.sendKeys('user');
+    username.sendKeys('cucumber_test');
     const password = await driver.findElement(By.name("password"));
-    password.sendKeys('pass', Key.ENTER);
+    password.sendKeys('741236985', Key.ENTER);
 });
 
-When('I see home button', async function(){
+When('I see home button', {timeout: 10 * 1000}, async function(){
     const condition = until.elementLocated(By.className("_8-yf5 "));
-    await driver.wait(condition, 7000);
+    await driver.wait(condition);
 });
 
-Then('I click in with {string}',  async function(home){
+Then('I click in with {string}',{timeout: 10 * 1000},  async function(home){
     const homepage = await driver.findElement(By.className("_8-yf5 ")).click();
-    //await driver.wait(condition, 7000);
+    //await driver.wait(homepage);
 });
 
-When('I see declaration button', async function(){
+When('I see declaration button',{timeout: 10 * 1000}, async function(){
     const condition = until.elementLocated(By.className('aOOlW   HoLwm '));
     await driver.wait(condition, 7000);
 });
 
-Then('I see button click in with {string}', async function(bildirim){
+Then('I see button click in with {string}',{timeout: 10 * 1000}, async function(bildirim){
     const bildirimbutonu = await driver.findElement(By.className('aOOlW   HoLwm ')).click();
-    //await driver.wait(condition, 7000);
+    //await driver.wait(bildirimbutonu);
 });
 
-When('I see message button', async function(){
-    const condition = until.elementLocated(By.className('xWeGp'));
-    await driver.wait(condition, 7000);
-});
-
-Then('I click in with message icon {string}', async function(mesaj){
-    const mesajlar = await driver.findElement(By.className('xWeGp'));
-    //await driver.wait(condition, 7000);
-});
-
-When('I see like button', async function(){
+When('I see like button',{timeout: 10 * 1000}, async function(){
     const condition = until.elementLocated(By.className('fr66n'));
-    //await driver.wait(condition, 7000);
+    await driver.wait(condition);
 });
 
-Then('I click in with like button {string}', async function(begen){
+Then('I click in with like button {string}',{timeout: 10 * 1000}, async function(begen){
     const begenibutonu = await driver.findElement(By.className('fr66n')).click();
-    await driver.wait(condition, 7000);
+    //await driver.wait(begenibutonu);
 });
